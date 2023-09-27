afad584d20ac42a033bfe769667cb9ecce694a7c
Pawan-88 <pavan@inexture.com>
2023-09-27 13:27:16 +0530

    Initial commit

---
+++ src/main/java/org/example/AWS_Lambda.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AWS_Lambda {
+
+    public static void main(String[] args){
+        SpringApplication.run(AWS_Lambda.class,args);
+    }
+}
---
+++ src/main/java/org/example/HelloLambda.java
@@ -0,0 +1,9 @@
+package org.example.test;
+
+public class HelloLambda {
+
+    public String handleRequest(){
+        return "Hello AWS Lambda!..";
+    }
+
+}
